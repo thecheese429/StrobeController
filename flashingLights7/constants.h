89c2b79e40a4a09555cdaadce0305870557598f5
thecheese429 <thecheese429@hotmail.com>
2016-07-12 21:06:56 -0500

    Update to functioning version using classes

--- flashingLights7/constants.h
+++ flashingLights7/constants.h
@@ -1,7 +1,132 @@
+#include <avr/pgmspace.h>
 #define MAXPINS 8
 
+#define LEFT 7
+#define CENTER 4
+#define BLUE 3
+#define RRED 6
+#define RIGHT 0
+
+#define MODE0 0
 #define MODE1 1
 #define MODE2 2
 #define MODE3 3
 #define MODE4 4
-#define MODE5 5 
+#define MODE5 5
+#define MODE6 6
+
+
+//define the pin number of the digital pins used for changing the mode
+#define MODE1PIN 0
+#define MODE2PIN 1
+#define MODE3PIN 2
+#define MODE4PIN 3
+
+#define UPDATECYCLES 10
+
+#define DEFAULTMODE MODE0
+
+
+// The number of channels used as output on the board: 8
+#define MAXPINS 8
+
+//define the millisecond intervals between toggling the state of a given channel
+//The 
+//the first value is discarded - enter any value then start actual interval with position 2
+const int mode0_1[] PROGMEM = {100, 10, -1};
+const int mode0_2[] PROGMEM = {100, 10, -1};
+const int mode0_3[] PROGMEM = {100, 10, -1};
+const int mode0_4[] PROGMEM = {100, 10, -1};
+const int mode0_5[] PROGMEM = {100, 10, -1};
+const int* const mode0_data[] PROGMEM = {mode0_1, mode0_2, mode0_3, mode0_4, mode0_5};
+const byte mode0_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+const int mode1_1[] PROGMEM = {10, 1000, -1};
+const int mode1_2[] PROGMEM = {20, 1000, -1};
+const int mode1_3[] PROGMEM = {30, 1000, -1};
+const int mode1_4[] PROGMEM = {40, 1000, -1};
+const int mode1_5[] PROGMEM = {50, 1000, -1};
+const int* const mode1_data[] PROGMEM = {mode1_1, mode1_2, mode1_3, mode1_4, mode1_5};
+const byte mode1_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+const int mode2_1[] PROGMEM = {500,250, -1};
+const int mode2_2[] PROGMEM = {40,20,40,20,40,20,40,20,40,20,40,1300, -1};
+const int mode2_3[] PROGMEM = {300, -1};
+const int mode2_4[] PROGMEM = {35,25,35,250,500,25,35,25,35,25,35,25,35, -1};
+const int mode2_5[] PROGMEM = {35,25,35,250,500,25,35,25,35,25,35,25,35, -1};
+const int* const mode2_data[] PROGMEM = {mode2_1, mode2_2, mode2_3, mode2_4};
+const byte mode2_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+const int mode3_1[] PROGMEM = {500,25,35,25,35,25,35,25,35,25,35,250,35, -1};
+const int mode3_2[] PROGMEM = {40,20,40,20,40,20,40,20,40,20,40,1300, -1};
+const int mode3_3[] PROGMEM = {300,30,30,30,30,30,30,30,30,600,35, -1};
+const int mode3_4[] PROGMEM = {35,25,35,250,500,25,35,25,35,25,35,25,35, -1};
+const int mode3_5[] PROGMEM = {35,25,35,250,500,25,35,25,35,25,35,25,35, -1};
+const int* const mode3_data[] PROGMEM = {mode3_1, mode3_2, mode3_3, mode3_4};
+const byte mode3_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+const int mode4_1[] PROGMEM = {1000, 0, -1};
+const int mode4_2[] PROGMEM = {1000, 0, -1};
+const int mode4_3[] PROGMEM = {1000, 0, -1};
+const int mode4_4[] PROGMEM = {1000, 0, -1};
+const int mode4_5[] PROGMEM = {1000, 0, -1};
+const int* const mode4_data[] PROGMEM = {mode4_1, mode4_2, mode4_3, mode4_4};
+const byte mode4_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+const int mode5_1[] PROGMEM = {10, 10, -1};
+const int mode5_2[] PROGMEM = {10, 10, -1};
+const int mode5_3[] PROGMEM = {10, 10, -1};
+const int mode5_4[] PROGMEM = {10, 10, -1};
+const int mode5_5[] PROGMEM = {10, 10, -1};
+const int* const mode5_data[] PROGMEM = {mode5_1, mode5_2, mode5_3, mode5_4, mode5_5};
+const byte mode5_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+const int mode6_1[] PROGMEM = {10, 10, -1};
+const int mode6_2[] PROGMEM = {10, 10, -1};
+const int mode6_3[] PROGMEM = {10, 10, -1};
+const int mode6_4[] PROGMEM = {10, 10, -1};
+const int mode6_5[] PROGMEM = {10, 10, -1};
+const int* const mode6_data[] PROGMEM = {mode6_1, mode6_2, mode6_3, mode6_4, mode6_5};
+const byte mode6_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+const int mode7_1[] PROGMEM = {10, 10, -1};
+const int mode7_2[] PROGMEM = {10, 10, -1};
+const int mode7_3[] PROGMEM = {10, 10, -1};
+const int mode7_4[] PROGMEM = {10, 10, -1};
+const int mode7_5[] PROGMEM = {10, 10, -1};
+const int* const mode7_data[] PROGMEM = {mode7_1, mode7_2, mode7_3, mode7_4, mode7_5};
+const byte mode7_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+const int mode8_1[] PROGMEM = {10, 10, -1};
+const int mode8_2[] PROGMEM = {10, 10, -1};
+const int mode8_3[] PROGMEM = {10, 10, -1};
+const int mode8_4[] PROGMEM = {10, 10, -1};
+const int mode8_5[] PROGMEM = {10, 10, -1};
+const int* const mode8_data[] PROGMEM = {mode8_1, mode8_2, mode8_3, mode8_4, mode8_5};
+const byte mode8_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+const int mode9_1[] PROGMEM = {10, 10, -1};
+const int mode9_2[] PROGMEM = {10, 10, -1};
+const int mode9_3[] PROGMEM = {10, 10, -1};
+const int mode9_4[] PROGMEM = {10, 10, -1};
+const int mode9_5[] PROGMEM = {10, 10, -1};
+const int* const mode9_data[] PROGMEM = {mode9_1, mode9_2, mode9_3, mode9_4, mode9_5};
+const byte mode9_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+const int mode10_1[] PROGMEM = {10, 10, -1};
+const int mode10_2[] PROGMEM = {10, 10, -1};
+const int mode10_3[] PROGMEM = {10, 10, -1};
+const int mode10_4[] PROGMEM = {10, 10, -1};
+const int mode10_5[] PROGMEM = {10, 10, -1};
+const int* const mode10_data[] PROGMEM = {mode10_1, mode10_2, mode10_3, mode10_4, mode10_5};
+const byte mode10_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+const int mode11_1[] PROGMEM = {10, 10, -1};
+const int mode11_2[] PROGMEM = {10, 10, -1};
+const int mode11_3[] PROGMEM = {10, 10, -1};
+const int mode11_4[] PROGMEM = {10, 10, -1};
+const int mode11_5[] PROGMEM = {10, 10, -1};
+const int* const mode11_data[] PROGMEM = {mode11_1, mode11_2, mode11_3, mode11_4, mode11_5};
+const byte mode11_pins[] PROGMEM = {LEFT, CENTER, BLUE, RRED, RIGHT, 255};//define the names of the given output pins
+
+
---
+++ flashingLights7/lights.ino
@@ -0,0 +1,178 @@
+#include "constants.h"
+// #include <BitBool.h>
+
+
+class Channel {
+  private:
+    const int* intervals;
+    int length;
+    byte pinNum;
+		
+    long previousTime;
+    int pos = 0;
+
+  public:
+    Channel(){
+      this->intervals = NULL;
+      this->length = 0;
+      this->pinNum = 0;
+      this->previousTime = 0;
+    }
+    Channel(const int* intervals, byte pinNum){
+      this->intervals = intervals;
+      this->length = 0;
+      this->pinNum = pinNum;
+      this->previousTime = 0;
+      pinMode(this->pinNum,OUTPUT);
+			Serial.print(" inter: ");
+			int j, val;
+      //calculate size of intervals array
+      while ( (int) pgm_read_word_near( intervals + this->length ) != -1) {
+				this->length++; 
+			}
+			
+			// Serial.print(" pin:");
+			// Serial.print(this->pinNum);
+			// Serial.print(" pos:");
+			// Serial.print(pos);
+			// Serial.print(" length:");
+			// Serial.print(this->length);
+			// Serial.println();
+    }
+    long update(long currTime) {
+			
+			long remainingTime;
+			// Serial.print(" curr:");
+			// Serial.print(currTime);
+			// Serial.print(" prev:");
+			// Serial.print(previousTime);
+			// Serial.print(" target:");
+			// Serial.print((int) pgm_read_word_near( intervals + pos ));
+			// Serial.print(" length:");
+			// Serial.print(this->length);
+			// Serial.println();
+			//check if channel state should be updated.
+      if ( remainingTime = (currTime - previousTime) >= (int) pgm_read_word_near( intervals + pos ) ) {
+        previousTime = currTime;
+				//if the current step in the interval sequence is odd, turn the pin on. Otherwise, turn it off.
+        if ( pos % 2 == 1) { 
+          digitalWrite(pinNum, HIGH);
+					// Serial.print("pin: ");
+					// Serial.print(pinNum);
+					// Serial.println(" Up.");
+        } else {
+          digitalWrite(pinNum, LOW);
+					// Serial.print("pin: ");
+					// Serial.print(pinNum);
+					// Serial.println(" Down.");
+        }
+				//if the current interval is the last one in the array of intervals, reset to index 0
+        if ( pos < this->length - 1 ) {
+          pos++;
+        } else {
+          pos = 0;
+        }
+      }
+			return currTime + remainingTime;
+    }
+};
+
+class Mode {
+	private:
+		Channel channels[MAXPINS];
+		byte channel_count;
+ public:
+  Mode() {
+    this->channel_count = 0;
+  }
+	Mode(const int* const* channels, const byte* pins) {
+		this->channel_count = 0;
+		//count channels
+		int i;
+    byte pin;
+		for (i=0; (pin = (byte)pgm_read_byte_near(pins + i)) != 255; i++){
+			// Serial.print("pinNum: ");
+			// Serial.print(pin);
+			// Serial.print(" ");
+			//make channel from pointer channel
+			this->channels[i] = Channel( (const int*)pgm_read_ptr_near(channels+i), pin );
+		}
+		this->channel_count = i;
+	}
+//TODO return soonest update time
+  long update(long t) {
+		long soonest=10000;
+		long time;
+    int i;
+    for (i=0; i < channel_count; i++) {
+			if ( (time = channels[i].update(t)) < soonest){
+				soonest = time;
+			}
+    }
+		return soonest;
+  }
+};
+
+// int modeNum = 0;
+Mode updateMode(Mode mode) {
+// int incomingByte = -1;
+int modeNum = -1;
+	// send data only when you receive data:
+ 	if (Serial.available() > 0) {
+		// read the incoming byte:
+		modeNum = Serial.read() - '0' ;
+
+		// say what you got:
+		Serial.print("I received: ");
+		Serial.println(modeNum, DEC);
+	}
+	
+	for(int i = 13; i>=8; i--){
+		if(digitalRead(i)==LOW){
+			modeNum = i-8;
+			break;
+		}
+	}
+	switch (modeNum) {
+		case 0:
+			mode = Mode(mode0_data, mode0_pins);
+			break;
+		case 1:
+			mode = Mode(mode1_data, mode1_pins);
+			break;
+		case 2:
+			mode = Mode(mode2_data, mode2_pins);
+			break;
+		default:
+			break;
+	}
+	Serial.print("Mode ");
+	Serial.print(modeNum);
+	Serial.println(" has been selected.");
+	return mode;
+}
+
+ Mode mode = Mode();
+
+void setup() {
+	// Serial.begin(9600);
+  // while (!Serial);
+	
+	for(int i = 13; i>=8; i--){
+		pinMode(i,INPUT_PULLUP);
+		}
+ 
+  Serial.println("seting mode to mode0");
+  mode = Mode(mode0_data, mode0_pins);
+  Serial.println("end of setup");
+}
+
+void loop() {
+	mode = updateMode(mode);
+	// waits until a pin needs to be updated.
+	static int soonest;
+	if (millis() > soonest) {
+		// TODO loop through each mode and set soonest to the smallest value.
+		mode.update(millis());
+	}
+}
